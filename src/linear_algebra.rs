ace2c597840c16c6eb9a56ab68a2a007a9e19818
Curtis Cali <curtiscali@pm.me>
2024-08-27 21:50:04 -0700

    More progress made

--- src/linear_algebra.rs
+++ src/linear_algebra.rs
@@ -1,18 +1,28 @@
+use std::fmt::Display;
+
+#[derive(Debug)]
 pub struct Vector2D {
     pub x: f64,
     pub y: f64
 }
 
+impl Display for Vector2D {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
+#[derive(Debug)]
 pub struct Line2D {
-    source: Vector2D,
-    destination: Vector2D
+    pub source: Vector2D,
+    pub direction: Vector2D
 }
 
 pub fn v2_sub(v1: &Vector2D, v2: &Vector2D) -> Vector2D {
     let Vector2D { x: x1, y: y1 } = v1;
     let Vector2D { x: x2, y: y2 } = v2;
 
-    return Vector2D { x: x1 - x2, y: y1 - y2};
+    return Vector2D { x: *x1 - *x2, y: *y1 - *y2};
 }
 
 pub fn v2_dotprod(v1: &Vector2D, v2: &Vector2D) -> f64 {
@@ -20,4 +30,22 @@ pub fn v2_dotprod(v1: &Vector2D, v2: &Vector2D) -> f64 {
     let Vector2D { x: x2, y: y2 } = v2;
 
     return (*x1 * *x2) + (*y1 * *y2);
+}
+
+pub fn v2_scalar_mult(v: &Vector2D, scalar: f64) -> Vector2D {
+    return Vector2D { x: scalar * v.x, y: scalar * v.y };
+}
+
+pub fn v2_get_normal_cw(v: &Vector2D) -> Vector2D {
+    return Vector2D { x: v.y, y: -v.x };
+}
+
+pub fn v2_get_normal_ccw(v: &Vector2D) -> Vector2D {
+    return Vector2D { x: -v.y, y: v.x };
+}
+
+
+pub fn v2_get_reflection_direction(incident_direction: &Vector2D, surface_normal_direction: &Vector2D) -> Vector2D {
+    let new_vector = v2_scalar_mult(&v2_scalar_mult(surface_normal_direction, 2.0), v2_dotprod(surface_normal_direction, incident_direction));
+    return v2_sub(incident_direction, &new_vector);
 }
--- src/problem/laser_beam_reflections.rs
+++ src/problem/laser_beam_reflections.rs
@@ -1,4 +1,20 @@
+use crate::linear_algebra::{
+    v2_get_normal_ccw, 
+    v2_get_normal_cw, 
+    v2_get_reflection_direction, 
+    Line2D, 
+    Vector2D
+};
+
 use super::Problem;
+
+fn slope_at_point_on_ellipse(x: f64, y: f64) -> Vector2D {
+    Vector2D { x: 1.0, y: (-4.0 * x) / y }
+}
+
+fn has_exited_the_ellipse(laser: &Line2D) -> bool {
+    return laser.source.y >= 10.0 && (laser.source.x >= -0.01 && laser.source.x <= 0.01);
+}
 
 pub struct LaserBeamReflectionsProblem {}
 
@@ -14,6 +30,49 @@ impl Problem for LaserBeamReflectionsProblem {
          * update vector = (dest(prev), intersection)
          */
 
-        return format!("No Solution Yet");
+        // This line corresponds to the initial (0, 10.1) -> (1.4, -9.6) where it strikes the mirror
+        let mut laser_beam = Line2D {
+            source: Vector2D { x: 1.4, y: -9.6 },
+            direction: Vector2D { x: 1.4, y: -19.7 }
+        };
+        
+        let mut num_reflections: u32 = 0;
+        while !has_exited_the_ellipse(&laser_beam) {
+            /* ALGO:
+             * calculate slope of elipse @ impact point
+             * determine which quadrant the impact point is, and use that to determine the normal vector
+             * use normal vector to calculate reflection direction vector
+             * update laser beam with new direction vector
+             * use reflection direction and laser source point to calculate next impact with ellipse
+             * update laser beam with new impact point
+             * add 1 to num_reflections
+             */
+            
+            let slope_of_ellipse_at_impact = slope_at_point_on_ellipse(laser_beam.source.x, laser_beam.source.y);
+            let mut slope_normal = Vector2D {x: 0.0, y: 0.0 };
+
+            if (laser_beam.source.x >= 0.0) {
+                if laser_beam.source.y <= 0.0 {
+                    slope_normal = v2_get_normal_cw(&slope_of_ellipse_at_impact);
+                } else {
+                    slope_normal = v2_get_normal_ccw(&slope_of_ellipse_at_impact);
+                }
+            } else {
+                if laser_beam.source.y <= 0.0 {
+                    slope_normal = v2_get_normal_ccw(&slope_of_ellipse_at_impact);
+                } else {
+                    slope_normal = v2_get_normal_cw(&slope_of_ellipse_at_impact);
+                }
+            }
+
+            let reflected_direction = v2_get_reflection_direction(&laser_beam.direction, &slope_normal);
+            println!("{} {} {}", slope_of_ellipse_at_impact, slope_normal, reflected_direction);
+            
+            // TODO: calculate next impact point of beam with new direction
+
+            num_reflections += 1;
+        }
+
+        return format!("{}", num_reflections);
     }
 }
