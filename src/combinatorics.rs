9f144a4b6e8c82ec1dea3735ab6a2102a0a79b13
Curtis Cali <curtiscali@pm.me>
2024-10-02 19:16:08 -0700

    Minor refactor to combinatorics

--- src/combinatorics.rs
+++ src/combinatorics.rs
@@ -1,5 +1,4 @@
-use std::clone;
-
+use std::cmp::{max, min};
 use num::Num;
 
 fn combinatoric_multiplier<T: Num + Clone + PartialOrd>(n: T, r: T) -> T {
@@ -17,25 +16,15 @@ fn combinatoric_multiplier<T: Num + Clone + PartialOrd>(n: T, r: T) -> T {
     return multiplier;
 }
 
-pub fn combinations<T: Num + Clone + PartialOrd>(n: T, r: T) -> T {
+pub fn combinations<T: Num + Clone + Ord>(n: T, r: T) -> T {
     if n < r {
         panic!("n must be less than r");
     } else if n == r || r == T::zero() {
         return T::one();
     }
 
-    let max = if r.clone() > (n.clone() - r.clone()) {
-        r.clone()
-    } else {
-        n.clone() - r.clone()
-    };
-
-
-    let min = if r.clone() < (n.clone() - r.clone()) {
-        r
-    } else {
-        n.clone() - r.clone()
-    };
+    let max = max(r.clone(), n.clone() - r.clone());
+    let min = min(r.clone(), n.clone() - r.clone());
 
     let numerator = combinatoric_multiplier(max.clone() + T::one(), n.clone());
     let denominator = combinatoric_multiplier(T::one(), min.clone());
