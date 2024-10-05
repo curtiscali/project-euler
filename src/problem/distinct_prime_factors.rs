066e5225bf2595a04fc46f35a553aefd9b100336
Curtis Cali <curtiscali@pm.me>
2024-10-04 22:31:23 -0700

    Optimized solution to #47

--- src/problem/distinct_prime_factors.rs
+++ src/problem/distinct_prime_factors.rs
@@ -1,5 +1,43 @@
-use crate::primes::prime_factors;
 use super::Problem;
+
+fn spf_sieve(n: usize) -> Vec<usize> {
+    let mut smallest_prime_factors = vec![1; n + 1];
+
+    let mut i = 2;
+    while i <= n {
+        if smallest_prime_factors[i] == 1 {
+            let mut j = i;
+            while j <= n {
+                if smallest_prime_factors[j] == 1 {
+                    smallest_prime_factors[j] = i;
+                }
+
+                j += i;
+            }
+        }
+
+        i += 1;
+    }
+
+    return smallest_prime_factors;
+}
+
+fn distinct_prime_factors_count(n: usize, smallest_prime_factors: &Vec<usize>) -> usize {
+    let mut factors_count = 0;
+
+    let mut most_recent_factor = 0;
+    let mut x = n;
+    while x != 1 {
+        if smallest_prime_factors[x] != most_recent_factor {
+            factors_count += 1;
+            most_recent_factor = smallest_prime_factors[x];
+        }
+
+        x /= smallest_prime_factors[x];
+    }
+
+    factors_count
+}
 
 pub struct DistinctPrimeFactorsProblem {}
 
@@ -7,11 +45,14 @@ impl Problem for DistinctPrimeFactorsProblem {
     fn solve(&self) -> String {
         const TARGET_FACTORS_COUNT: usize = 4;
 
-        let mut found_numbers: Vec<u32> = vec![];
+        // 250k is the upper bound since I know the answer will be in the low 6 digits
+        let smallest_prime_factors = spf_sieve(250_000);
+
+        let mut found_numbers: Vec<usize> = vec![];
         let mut n = 647;
 
         while found_numbers.len() < TARGET_FACTORS_COUNT {
-            let distinct_prime_factors_count = prime_factors(n).into_keys().count();
+            let distinct_prime_factors_count = distinct_prime_factors_count(n, &smallest_prime_factors);
 
             if distinct_prime_factors_count == TARGET_FACTORS_COUNT {
                 if found_numbers.len() == 0 {
@@ -27,7 +68,6 @@ impl Problem for DistinctPrimeFactorsProblem {
 
             n += 1;
         }
-
 
         format!("{:?}", found_numbers)
     }
