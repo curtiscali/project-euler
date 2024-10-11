734fcc57506541c4b500a12b4d05d7c33a0b8433
Curtis Cali <curtiscali@pm.me>
2024-10-11 10:30:19 -0700

    Removed unused functions from #179

--- src/problem/consecutive_positive_divisors.rs
+++ src/problem/consecutive_positive_divisors.rs
@@ -1,45 +1,6 @@
 use std::collections::HashMap;
 use super::Problem;
 
-fn spf_sieve(n: usize) -> Vec<usize> {
-    let mut smallest_prime_factors = vec![1; n + 1];
-
-    let mut i = 2;
-    while i <= n {
-        if smallest_prime_factors[i] == 1 {
-            let mut j = i;
-            while j <= n {
-                if smallest_prime_factors[j] == 1 {
-                    smallest_prime_factors[j] = i;
-                }
-
-                j += i;
-            }
-        }
-
-        i += 1;
-    }
-
-    return smallest_prime_factors;
-}
-
-fn sigma_0(n: usize, smallest_prime_factors: &Vec<usize>) -> usize {
-    let mut prime_factors: HashMap<usize, usize> = HashMap::new();
-
-    let mut x = n;
-    while x != 1 {
-        let p = smallest_prime_factors[x];
-
-        prime_factors.entry(p)
-            .and_modify(|k| *k = *k + 1)
-            .or_insert(1);
-
-        x /= p;
-    }
-
-    prime_factors.into_iter().map(|(_, k)| k + 1).product::<usize>()
-}
-
 pub struct ConsecutivePositiveDivisorsProblem {
     pub upper_bound: u32
 }
