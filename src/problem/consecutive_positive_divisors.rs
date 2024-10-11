ecf9ee8636a8f46d7de3432fbb6ca90798be7b61
Curtis Cali <curtiscali@pm.me>
2024-10-11 10:28:13 -0700

    Optimized performance of #179 solution

--- src/problem/consecutive_positive_divisors.rs
+++ src/problem/consecutive_positive_divisors.rs
@@ -46,12 +46,22 @@ pub struct ConsecutivePositiveDivisorsProblem {
 
 impl Problem for ConsecutivePositiveDivisorsProblem {
     fn solve(&self) -> String {
-        let smallest_prime_factors = spf_sieve(self.upper_bound as usize);
+        let mut sigma0_lookup = vec![1u32; self.upper_bound as usize];
+        let mut i = 2;
+        while i < sigma0_lookup.len() {
+            let mut j = i;
+            while j < sigma0_lookup.len() {
+                sigma0_lookup[j] += 1;
+                j += i;
+            }
+
+            i += 1;
+        }
 
         let mut num_consecutive = 0u32;
         let mut n = 2;
         while n < self.upper_bound {
-            if sigma_0(n as usize, &smallest_prime_factors) == sigma_0((n + 1) as usize, &smallest_prime_factors) {
+            if sigma0_lookup[n as usize] == sigma0_lookup[(n - 1) as usize] { 
                 num_consecutive += 1;
             }
 
