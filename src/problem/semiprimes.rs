f4b01add602528f378607498f1cffc6ce0afe167
Curtis Cali <curtiscali@pm.me>
2024-10-13 13:42:07 -0700

    Optimized solution for #187

--- src/problem/semiprimes.rs
+++ src/problem/semiprimes.rs
@@ -1,20 +1,37 @@
-use crate::primes::sieve_of_atkin;
+use num::integer::Roots;
+use crate::primes::primes_below;
 use super::Problem;
 
-fn pi(n: u32, primes: &Vec<u32>) -> u32 {
-    let mut primes_count = 0;
-    let mut i = 0;
-    while i < primes.len() {
-        if primes[i] > n {
-            break;
-        }
-
-        primes_count += 1;
-
-        i += 1;
+fn phi(x: u32, a: u32, primes: &Vec<u32>) -> u32 {
+    if a < 1 {
+        return x;
     }
 
-    primes_count
+    if a == 1 {
+        return x - (x >> 1);
+    }
+
+    let pa = primes[(a as usize) - 1];
+    if x <= pa {
+        return 1;
+    }
+
+    phi(x, a - 1, primes) - phi(x / pa, a - 1, primes)
+}
+
+fn pi(n: u32) -> u32 {
+    if n < 2 {
+        return 0;
+    }
+
+    if n == 2 {
+        return 1;
+    }
+
+    let primes = primes_below::<u32>(n.sqrt() as usize);
+    let a = primes.len() as u32;
+
+    phi(n, a, &primes) + a - 1
 }
 
 pub struct SemiprimesProblem {
@@ -23,29 +40,14 @@ pub struct SemiprimesProblem {
 
 impl Problem for SemiprimesProblem {
     fn solve(&self) -> String {
-        // This function implements teh Semiprime counting function https://en.wikipedia.org/wiki/Semiprime#Formula_for_number_of_semiprimes
-        let primes_below_half_lookup = sieve_of_atkin((self.n / 2) as usize);
-        let mut primes_below_half: Vec<u32> = vec![];
-
-        let mut i = 2;
-        while i < primes_below_half_lookup.len() {
-            if primes_below_half_lookup[i] {
-                primes_below_half.push(i as u32);
-            }
-
-            i += 1;
-        }
-
-        let pi_sqrt = pi((self.n as f64).sqrt() as u32, &primes_below_half);
+        // This function implements the Semiprime counting function https://en.wikipedia.org/wiki/Semiprime#Formula_for_number_of_semiprimes
+        let root = self.n.sqrt();
+        let primes = primes_below::<u32>(root as usize);
 
         let mut num_semiprimes = 0u32;
-        let mut k = 1;
-        while k <= pi_sqrt {
-            let p = primes_below_half[(k as usize) - 1];
-
-            num_semiprimes += pi(self.n / p, &primes_below_half) - k + 1;
-
-            k += 1;
+        for k in 1..=primes.len() {
+            let p = primes[k - 1];
+            num_semiprimes += pi(self.n / p) - (k as u32) + 1;
         }
 
         format!("{}", num_semiprimes)
