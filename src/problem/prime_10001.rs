c5df144aa2dbf6792e3161453e2027e9326e7055
Curtis Cali <curtiscali@pm.me>
2024-10-16 21:10:07 -0700

    Slightly optimized #7 solution

--- src/problem/prime_10001.rs
+++ src/problem/prime_10001.rs
@@ -7,15 +7,15 @@ pub struct NthPrimeProblem {
 
 impl Problem for NthPrimeProblem {
     fn solve(&self) -> String {
-        // Using the asymptotic bounds nln(n)+n(ln(ln)n))−1) < pn < nln(n)+nln(ln(n)) for n >= 6
-        // This allows us to compute primes using the sieve of eratosthenes instead of brute-forcing
-        // by testing each n for primality, which becomes more and more expensive as the numbers grow
+        // Using the asymptotic bounds nln(n)+n(ln(ln(n))−1) < pn < nln(n)+nln(ln(n)) for n >= 6
+        // We can generate a list of primes below that upper bound for the nth primes
+        // then look at index n - 1 to find our target prime
         let float_n = self.n as f64;
         let ln = float_n.ln();
         let float_upper_bound = (float_n * ln) + (float_n * ln.ln());
         let upper_bound = float_upper_bound as usize;
 
-        let primes = primes_below::<u32>(upper_bound);//sieve_of_eratosthenes(upper_bound);
+        let primes = primes_below::<u32>(upper_bound);
 
         return format!("{}", primes[(self.n - 1) as usize]);
     }
