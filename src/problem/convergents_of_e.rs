d60ca9dc00afedccab63cf6feb533438bb4a1d4f
Curtis Cali <curtiscali@pm.me>
2024-10-20 12:10:15 -0700

    Minor optimizations to #65: replaced vec with array and made a(n) return u32

--- src/problem/convergents_of_e.rs
+++ src/problem/convergents_of_e.rs
@@ -4,9 +4,9 @@ use super::Problem;
 
 // based on the recurrence relation for a(n) defined here:
 // https://proofwiki.org/wiki/Continued_Fraction_Expansion_of_Euler%27s_Number
-fn a(n: &BigInt) -> BigInt {
-    if n % 3 == BigInt::ZERO || n % 3 == BigInt::from(2) {
-        return BigInt::one();
+fn a(n: u32) -> u32 {
+    if n % 3 == 0 || n % 3 == 2 {
+        return 1;
     }
 
     2 * ((n - 1) / 3)
@@ -20,12 +20,10 @@ impl Problem for ConvergentsOfEProblem {
         // https://proofwiki.org/wiki/Continued_Fraction_Expansion_of_Euler%27s_Number
         // pi = ai * p(i-1) + p(i-2)
         // qi = ai * q(i-1) + q(i-2)
-        let mut prev_ps = vec![BigInt::one(), BigInt::one()];
+        let mut prev_ps = [BigInt::one(), BigInt::one()];
 
         for i in 2..102 {
-            let n = BigInt::from(i);
-
-            let ai = a(&n);
+            let ai = a(i);
             let new_p = (&ai * &prev_ps[1]) + &prev_ps[0];
 
             prev_ps[0] = prev_ps[1].clone();
