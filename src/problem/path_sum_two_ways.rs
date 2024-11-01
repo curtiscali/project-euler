029aaa43bfda4ca616d69b6de319b95871c3f938
Curtis Cali <curtiscali@pm.me>
2024-11-02 10:42:48 +1100

    minor code polishing

--- src/problem/path_sum_two_ways.rs
+++ src/problem/path_sum_two_ways.rs
@@ -1,5 +1,4 @@
 use std::cmp::min;
-
 use super::Problem;
 
 pub struct PathSumTwoWaysProblem {}
@@ -8,7 +7,7 @@ impl Problem for PathSumTwoWaysProblem {
     fn solve(&self) -> String {
         const GRID_DIM: usize = 80;
 
-        let mut grid: Vec<Vec<u32>> = vec![];
+        let mut grid = vec![];
         let mut distances = [[0; GRID_DIM]; GRID_DIM];
 
         let bytes = include_bytes!("../data_files/0081_matrix.txt");
@@ -20,12 +19,17 @@ impl Problem for PathSumTwoWaysProblem {
             );
         }
 
+        // there's only one way to get to the top-left corner, so init that to the top-left of the grid
         distances[0][0] = grid[0][0];
 
+        // The only way to get to the leftmost column is from the row above
+        // So we init that to the distance of the elements above plus current grid element
         for i in 1..grid.len() {
             distances[i][0] = distances[i - 1][0] + grid[i][0];
         }
 
+        // The only way to get to the top row is from the left
+        // So we init that to the distance of the elements to the left plus current grid element
         for j in 1..grid[0].len() {
             distances[0][j] = distances[0][j - 1] + grid[0][j];
         }
