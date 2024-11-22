44ef2c2f77600fc9f05014e4d5016beffa0341bc
Curtis Cali <curtiscali@pm.me>
2024-11-22 15:45:32 -0800

    ensured problems list remains sorted

--- src/main.rs
+++ src/main.rs
@@ -1,103 +1,71 @@
-use std::time::Instant;
 use clap::Parser;
 use inquire::Select;
 use problem::{
-    amicable_numbers::AmicableNumbersProblem,
-    bouncy_numbers::BouncyNumbersProblem, 
-    champernownes_constant::ChampernownesConstantProblem, 
-    circular_primes::CircularPrimesProblem, 
-    coded_triangle_numbers::CodedTriangleNumbersProblem, 
-    coin_partitions::CoinPartitionsProblem,
-    coin_sums::CoinSumProblem,
-    combinatoric_selections::CombinatoricSelectionsProblem, 
+    amicable_numbers::AmicableNumbersProblem, bouncy_numbers::BouncyNumbersProblem,
+    champernownes_constant::ChampernownesConstantProblem, circular_primes::CircularPrimesProblem,
+    coded_triangle_numbers::CodedTriangleNumbersProblem, coin_partitions::CoinPartitionsProblem,
+    coin_sums::CoinSumProblem, combinatoric_selections::CombinatoricSelectionsProblem,
     concealed_squares::ConcealedSquaresProblem,
-    consecutive_positive_divisors::ConsecutivePositiveDivisorsProblem, 
-    consecutive_prime_sum::ConsecutivePrimeSumProblem, 
-    convergents_of_e::ConvergentsOfEProblem, 
-    counting_fractions::CountingFractionsProblem, 
-    counting_fractions_in_a_range::CountingFractionsInARangeProblem, 
-    counting_rectangles::CountingRectanglesProblem, 
-    counting_summations::CountingSummationsProblem, 
-    counting_sundays::CountingSundaysProblem, 
-    digit_factorials::DigitFactorialsProblem, 
-    digit_factorial_chains::DigitFactorialChainsProblem,
-    digit_fifth_powers::DigitFifthPowersProblem, 
-    distinct_powers::DistinctPowersProblem, 
-    distinct_prime_factors::DistinctPrimeFactorsProblem, 
-    double_base_palindrome::DoubleBasePalindromeProblem, 
-    efficient_exponentiation::EfficientExponentiationProblem, 
-    even_fibonacci::EvenFibonacciProblem, 
-    factorial_digit_sum::FactorialDigitSum, 
-    goldbachs_other_conjecture::GoldbachsOtherConjectureProblem, 
-    highly_divisible_triangle_number::HighlyDivisibleTriangleNumber, 
-    hyperexponentiation::HyperexponentiationProblem, 
-    integer_right_triangles::IntegerRightTrianglesProblem, 
-    large_non_mersenne_prime::LargeNonMersennePrimeProblem, 
-    large_sum::LargeSumProblem, 
-    largest_exponential::LargestExponentialProblem, 
-    largest_palindrome_product::LargestPalindromeProduct, 
-    largest_prime_factor::LargestPrimeFactorProblem, 
-    largest_product::LargestProductProblem, 
-    largest_product_in_a_grid::LargestProductInAGridProblem, 
-    laser_beam_reflections::LaserBeamReflectionsProblem, 
-    lattice_paths::LatticePathsProblem, 
-    lexicographic_permutations::LexicographicPermutationsProblem, 
-    longest_collatz_sequence::LongestCollatzSequenceProblem, 
-    lychrel_numbers::LychrelNumbersProblem, 
-    maximum_path_sum_one::MaximumPathSumOneProblem, 
-    maximum_path_sum_two::MaximumPathSumTwoProblem, 
-    multiples::MultiplesProblem, 
-    names_scores::NamesScoresProblem, 
-    non_abundant_sums::NonAbundantSumsProblem,
-    non_bouncy_numbers::NonBouncyNumbersProblem,
-    number_letter_counts::NumberLetterCountsProblem, 
+    consecutive_positive_divisors::ConsecutivePositiveDivisorsProblem,
+    consecutive_prime_sum::ConsecutivePrimeSumProblem, convergents_of_e::ConvergentsOfEProblem,
+    counting_fractions::CountingFractionsProblem,
+    counting_fractions_in_a_range::CountingFractionsInARangeProblem,
+    counting_rectangles::CountingRectanglesProblem, counting_summations::CountingSummationsProblem,
+    counting_sundays::CountingSundaysProblem, digit_factorial_chains::DigitFactorialChainsProblem,
+    digit_factorials::DigitFactorialsProblem, digit_fifth_powers::DigitFifthPowersProblem,
+    distinct_powers::DistinctPowersProblem, distinct_prime_factors::DistinctPrimeFactorsProblem,
+    double_base_palindrome::DoubleBasePalindromeProblem,
+    efficient_exponentiation::EfficientExponentiationProblem, even_fibonacci::EvenFibonacciProblem,
+    factorial_digit_sum::FactorialDigitSum,
+    goldbachs_other_conjecture::GoldbachsOtherConjectureProblem,
+    highly_divisible_triangle_number::HighlyDivisibleTriangleNumber,
+    hyperexponentiation::HyperexponentiationProblem,
+    integer_right_triangles::IntegerRightTrianglesProblem,
+    large_non_mersenne_prime::LargeNonMersennePrimeProblem, large_sum::LargeSumProblem,
+    largest_exponential::LargestExponentialProblem,
+    largest_palindrome_product::LargestPalindromeProduct,
+    largest_prime_factor::LargestPrimeFactorProblem, largest_product::LargestProductProblem,
+    largest_product_in_a_grid::LargestProductInAGridProblem,
+    laser_beam_reflections::LaserBeamReflectionsProblem, lattice_paths::LatticePathsProblem,
+    lexicographic_permutations::LexicographicPermutationsProblem,
+    longest_collatz_sequence::LongestCollatzSequenceProblem,
+    lychrel_numbers::LychrelNumbersProblem, maximum_path_sum_one::MaximumPathSumOneProblem,
+    maximum_path_sum_two::MaximumPathSumTwoProblem, multiples::MultiplesProblem,
+    names_scores::NamesScoresProblem, non_abundant_sums::NonAbundantSumsProblem,
+    non_bouncy_numbers::NonBouncyNumbersProblem, number_letter_counts::NumberLetterCountsProblem,
     number_power_spirals::NumberPowerSpiralsProblem,
-    odd_period_square_roots::OddPeriodSquareRootsProblem, 
-    ordered_fractions::OrderedFractionsProblem, 
-    ordered_radicals::OrderedRadicalsProblem, 
-    pandigital_multiples::PandigitalMultiplesProblem, 
-    pandigital_prime::PandigitalPrimeProblem, 
-    pandigital_products::PandigitalProductsProblem, 
-    passcode_derivation::PasscodeDerivationProblem, 
-    path_sum_two_ways::PathSumTwoWaysProblem,
-    path_sum_three_ways::PathSumThreeWaysProblem,
-    path_sum_four_ways::PathSumFourWaysProblem,
-    permuted_multiples::PermutedMultiplesProblem,
-    power_digit_sum::PowerDigitSum, 
-    powerful_digit_counts::PowerfulDigitCountsProblem, 
-    powerful_digit_sum::PowerfulDigitSumProblem, 
-    prime_10001::NthPrimeProblem, 
-    prime_permutations::PrimePermutationsProblem,
-    prime_summations::PrimeSummationsProblem,
-    reciprocal_cycles::ReciprocalCyclesProblem, 
-    reversible_numbers::ReversibleNumbersProblem, 
-    roman_numerals::RomanNumeralsProblem, 
-    self_powers::SelfPowersProblem, 
-    semiprimes::SemiprimesProblem, 
-    smallest_multiple::SmallestMultipleProblem, 
-    special_pythagorean_triplet::SpecialPythagoreanTripletProblem, 
-    spiral_primes::SpiralPrimesProblem, 
-    square_digit_chains::SquareDigitChainsProblem, 
-    square_root_convergents::SquareRootConvergentsProblem, 
-    sum_of_squares_of_divisors::SumOfSquaresOfDivisorsProblem, 
-    sum_square_difference::SumSquareDifference, 
-    summation_of_primes::SummationOfPrimes, 
-    thousand_digit_fibonacci_number::ThousandDigitFibonacciNumberProblem, 
-    totient_maximum::TotientMaximumProblem, 
-    totient_permutation::TotientPermutationProblem, 
-    triangle_containment::TriangleContainmentProblem, 
-    triangular_pentagonal_hexagonal::TriangularPentagonalHexagonalNumberProblem,
-    Problem
+    odd_period_square_roots::OddPeriodSquareRootsProblem,
+    ordered_fractions::OrderedFractionsProblem, ordered_radicals::OrderedRadicalsProblem,
+    pandigital_multiples::PandigitalMultiplesProblem, pandigital_prime::PandigitalPrimeProblem,
+    pandigital_products::PandigitalProductsProblem, passcode_derivation::PasscodeDerivationProblem,
+    path_sum_four_ways::PathSumFourWaysProblem, path_sum_three_ways::PathSumThreeWaysProblem,
+    path_sum_two_ways::PathSumTwoWaysProblem, permuted_multiples::PermutedMultiplesProblem,
+    power_digit_sum::PowerDigitSum, powerful_digit_counts::PowerfulDigitCountsProblem,
+    powerful_digit_sum::PowerfulDigitSumProblem, prime_10001::NthPrimeProblem,
+    prime_permutations::PrimePermutationsProblem, prime_summations::PrimeSummationsProblem,
+    reciprocal_cycles::ReciprocalCyclesProblem, reversible_numbers::ReversibleNumbersProblem,
+    roman_numerals::RomanNumeralsProblem, self_powers::SelfPowersProblem,
+    semiprimes::SemiprimesProblem, smallest_multiple::SmallestMultipleProblem,
+    special_pythagorean_triplet::SpecialPythagoreanTripletProblem,
+    spiral_primes::SpiralPrimesProblem, square_digit_chains::SquareDigitChainsProblem,
+    square_root_convergents::SquareRootConvergentsProblem,
+    sum_of_squares_of_divisors::SumOfSquaresOfDivisorsProblem,
+    sum_square_difference::SumSquareDifference, summation_of_primes::SummationOfPrimes,
+    thousand_digit_fibonacci_number::ThousandDigitFibonacciNumberProblem,
+    totient_maximum::TotientMaximumProblem, totient_permutation::TotientPermutationProblem,
+    triangle_containment::TriangleContainmentProblem,
+    triangular_pentagonal_hexagonal::TriangularPentagonalHexagonalNumberProblem, Problem,
 };
+use std::time::Instant;
 
-pub mod problem;
-pub mod primes;
-pub mod number_theory;
-pub mod strings;
 pub mod combinatorics;
-pub mod linear_algebra;
 pub mod fractions;
 pub mod hyperops;
+pub mod linear_algebra;
+pub mod number_theory;
+pub mod primes;
+pub mod problem;
+pub mod strings;
 pub mod triangles;
 
 /// Simple program to greet a person:104
@@ -106,7 +74,7 @@ pub mod triangles;
 struct Args {
     /// Number of the problem for which you would like a solution as referenced on https://projecteuler.net
     #[arg(short, long)]
-    problem: Option<u16>
+    problem: Option<u16>,
 }
 
 fn print_solution(problem: &dyn Problem) {
@@ -138,13 +106,13 @@ fn print_solution(problem: &dyn Problem) {
 fn main() {
     let args = Args::parse();
 
-    let solved_problems = vec![
+    let mut solved_problems = vec![
         Box::new(MultiplesProblem {}) as Box<dyn Problem>,
         Box::new(EvenFibonacciProblem {}),
         Box::new(LargestPrimeFactorProblem {}),
         Box::new(LargestPalindromeProduct {}),
         Box::new(SmallestMultipleProblem {}),
-        Box::new(SumSquareDifference {}) ,
+        Box::new(SumSquareDifference {}),
         Box::new(NthPrimeProblem {}),
         Box::new(LargestProductProblem {}),
         Box::new(SpecialPythagoreanTripletProblem {}),
@@ -226,6 +194,10 @@ fn main() {
         Box::new(SumOfSquaresOfDivisorsProblem {}),
     ];
 
+    // ensure the problems list is sorted by number
+    // If the list gets too long in the future, we can ensure it's manually
+    // sorted above to save the computation
+    solved_problems.sort_by(|a, b| a.number().cmp(&b.number()));
 
     match args.problem {
         Some(problem_number) => {
@@ -233,23 +205,25 @@ fn main() {
                 Ok(idx_of_problem) => {
                     let selected_problem = &solved_problems[idx_of_problem];
                     print_solution(selected_problem.as_ref());
-                }, 
+                }
                 Err(_) => {
                     println!("Problem {} has not yet been solved", problem_number);
                 }
             }
-        },
+        }
         None => {
             let selection = Select::new(
-                "Select the problem for which you'd like to see the solution from the list below", 
-                solved_problems
-            ).with_vim_mode(true).prompt();
+                "Select the problem for which you'd like to see the solution from the list below",
+                solved_problems,
+            )
+            .with_vim_mode(true)
+            .prompt();
 
             match selection {
                 Ok(selected_problem) => {
                     print_solution(selected_problem.as_ref());
-                },
-                Err(_) => println!("Must select a problem.")
+                }
+                Err(_) => println!("Must select a problem."),
             }
         }
     }
